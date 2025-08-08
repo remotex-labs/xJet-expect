c07301daba003d96828850d7afddf549a6aba6d5
Garefild <Garefild2@gmail.com>
2025-08-08 21:26:36 +0300

    tsconfig: add alias

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,5 +9,14 @@ module.exports = {
     ],
     testPathIgnorePatterns: [ '/lib/', '/node_modules/', '/dist/' ],
     moduleNameMapper: {
+        '^@errors/(.*)$': '<rootDir>/src/errors/$1',
+        '^@handlers/(.*)$': '<rootDir>/src/handlers/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@providers/(.*)$': '<rootDir>/src/providers/$1',
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+        '^@diff/(.*)$': '<rootDir>/src/modules/diff/$1',
+        '^@matchers/(.*)$': '<rootDir>/src/providers/matchers/$1',
+        '^@patterns/(.*)$': '<rootDir>/src/providers/patterns/$1'
     },
 };
